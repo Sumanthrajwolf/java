11bae55f156d8bee31f1b56a719c7b489f97e57b
sumanthrajwolf <sumanthraj.works@gmail.com>
2023-05-09 22:15:22 +0700

    project

---
+++ JavaBasics/src/Basics.java
@@ -0,0 +1,44 @@
+// 
+public class Basics {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		
+		//variable 
+		int i = 20;
+		short s = 30;
+		long l = 40;
+		float f = 50;
+		
+		String c = "name";
+		
+		char mychar = 'y';
+		boolean b = true;
+		 
+		double d = 7.434;
+		float fl = 0.456f;
+		
+		
+		//types of byte, short, long
+		
+		
+		
+		System.out.println(i);
+		System.out.println(s);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(c);
+		System.out.println(mychar);
+		System.out.println(b);
+		System.out.println(d);
+		System.out.println(fl);
+		
+		
+		
+		
+		
+
+	}
+
+}
